9a06f66864f7071f7ea7533e4a356e811df983bd
Sushmit360 <118396880+Sushmit360@users.noreply.github.com>
2024-03-06 17:30:48 -0500

    Deployment

--- app.py
+++ app.py
@@ -233,4 +233,4 @@ def delete_budget(budget_id):
 # Make sure to implement these based on your application's needs
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
